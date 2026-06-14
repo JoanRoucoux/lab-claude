5c10420407067f5fde0b4d098db2ba2aa5798f9f
Joan Roucoux <joan.roucoux@gmail.com>
2026-06-14 15:32:28 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/joanroucoux/lab_claude/LabClaudeApplication.java
@@ -0,0 +1,13 @@
+package com.joanroucoux.lab_claude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabClaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabClaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joanroucoux/lab_claude/LabClaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joanroucoux.lab_claude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabClaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
